115f4ec9080b9fb245dca095610fef126ff5bbe2
Pedro Papadópolis <pplp93@gmail.com>
2023-03-05 16:07:35 +1100

    Add CF function association to cache behaviour

--- .cicd/terraform/cloudfront.tf
+++ .cicd/terraform/cloudfront.tf
@@ -42,33 +42,16 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.redirect_to_index.arn
+    }
   }
 
   # Cache behavior with precedence 0
   ordered_cache_behavior {
     path_pattern     = "/images/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  ordered_cache_behavior {
-    path_pattern     = "/store/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.s3_origin_id
@@ -104,6 +87,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.redirect_to_index.arn
+    }
+
     min_ttl                = 0
     default_ttl            = 86400
     max_ttl                = 31536000
@@ -129,4 +117,25 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn = aws_acm_certificate_validation.brz_gg_cert.certificate_arn
     ssl_support_method = "sni-only"
   }
+}
+
+resource "aws_cloudfront_function" "redirect_to_index" {
+  name    = "redirect-to-index"
+  runtime = "cloudfront-js-1.0"
+  comment = ""
+  publish = true
+  code    = <<EOF
+    function handler(event) {
+      var request = event.request;
+      var uri = request.uri;
+      
+      if (uri.endsWith('/')) {
+          request.uri += 'index.html';
+      } else if (!uri.includes('.')) {
+          request.uri += '/index.html';
+      }
+
+      return request;
+    }
+  EOF
 }
