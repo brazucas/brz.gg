0050d0b655c631bb2ae0496c8582c6d4e96cabc9
pedropapa <pedro.papadopolis@gmail.com>
2023-10-03 12:48:58 +1100

    feat: show user nickname & profile

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
 import { useAuthToken } from "./useAuthToken";
 
+type UserInfo = {
+    nickname: string;
+    name: string;
+    email: string;
+    picture: string;
+    email_verified: string;
+}
+
 export const useUser = () => {
     const { authToken, authTokenExpiresIn } = useAuthToken();
     const [ isUserAuthenticated, setIsAuthenticated ] = useState(false);
+    const [ userInfo, setUserInfo ] = useState<UserInfo | null>(null);
 
     useEffect(() => {
         const isTokenExpired = new Date() > new Date(authTokenExpiresIn);
         setIsAuthenticated(authToken && !isTokenExpired);
     }, [authToken, authTokenExpiresIn]);
 
+    useEffect(() => {
+        if(isUserAuthenticated) {
+            fetch(`https://${import.meta.env.PUBLIC_AUTH0_DOMAIN}/userinfo`, {
+                headers: {
+                    Authorization: `Bearer ${authToken}`
+                }
+                })
+                .then(response => response.json())
+                .then(data => setUserInfo(data)
+            );
+        }
+    }, [isUserAuthenticated]);
+
     return {
-        isUserAuthenticated
+        isUserAuthenticated, userInfo
     };
 };
