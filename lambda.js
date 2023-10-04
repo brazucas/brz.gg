e66bfb4b20bea36aaeaaf97274238f112681abe4
pedropapa <pedro.papadopolis@gmail.com>
2023-10-04 20:36:05 +1100

    feat: lambda handler

---
+++ lambda.js
@@ -0,0 +1,35 @@
+import { createServer, proxy } from "aws-serverless-express";
+import { eventContext } from "aws-serverless-express/middleware.js";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
+
+import express from "express";
+
+const binaryMimeTypes = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/gif",
+  "image/svg+xml",
+  "image/svg",
+];
+
+let cachedServer;
+
+async function bootstrapServer() {
+  const base = "/";
+
+  if (!cachedServer) {
+    const expressApp = express();
+    expressApp.use(base, express.static("dist/client/"));
+    expressApp.use(eventContext());
+    expressApp.use(ssrHandler);
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+
+  return cachedServer;
+}
+
+export const handler = async (event, context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, "PROMISE").promise;
+};
