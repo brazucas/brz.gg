325788d9b2034b844fb9a61ee2f621cd5c7b87bc
pedropapa <pedro.papadopolis@gmail.com>
2023-10-04 20:30:45 +1100

    feat: add support to refreshtoken

--- src/hooks/useAuthToken.tsx
+++ src/hooks/useAuthToken.tsx
@@ -2,29 +2,33 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 export const useAuthToken = () => {
-    const [cookies, setCookie, removeCookie] = useCookies(['authToken', 'authTokenExpiresIn', 'authTokenType']);
+    const [cookies, setCookie, removeCookie] = useCookies(['authToken', 'authTokenExpiresIn', 'authTokenType', 'refreshToken']);
     const [authToken, updateAuthToken] = useState(null);
+    const [refreshToken, updateRefreshToken] = useState(null);
     const [authTokenExpiresIn, updateAuthTokenExpiresIn] = useState(null);
     const [authTokenType, updateTokenType] = useState(null);
 
     const setAuthToken = (token: string) => setCookie('authToken', token);
+    const setRefreshToken = (type: string) => setCookie('refreshToken', type);
     const setAuthTokenExpiresIn = (expiresIn: string) => setCookie('authTokenExpiresIn', expiresIn);
     const setAuthTokenType = (type: string) => setCookie('authTokenType', type);
 
     const removeAuthTokens = () => {
-        removeCookie('authToken');
-        removeCookie('authTokenExpiresIn');
-        removeCookie('authTokenType');
+        removeCookie('authToken', {path:'/'});
+        removeCookie('authTokenExpiresIn', {path:'/'});
+        removeCookie('authTokenType', {path:'/'});
+        removeCookie('refreshToken', {path:'/'});
     };
     
     useEffect(() => {
-        const { authToken, authTokenExpiresIn, authTokenType } = cookies;
+        const { authToken, authTokenExpiresIn, authTokenType, refreshToken } = cookies;
         updateAuthToken(authToken);
+        updateRefreshToken(refreshToken);
         updateAuthTokenExpiresIn(authTokenExpiresIn);
         updateTokenType(authTokenType);
     }, [cookies]);
 
     return {
-        authToken, setAuthToken, authTokenExpiresIn, setAuthTokenExpiresIn, authTokenType, setAuthTokenType, removeAuthTokens
+        authToken, setAuthToken, authTokenExpiresIn, setAuthTokenExpiresIn, authTokenType, setAuthTokenType, removeAuthTokens, refreshToken, setRefreshToken
     };
 };
