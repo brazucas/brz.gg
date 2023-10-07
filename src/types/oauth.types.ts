1f74bb801c264a09825614dcbb8bd0e853040c41
pedropapa <pedro.papadopolis@gmail.com>
2023-10-07 13:01:01 +1100

    feat: get user info as part of the authenticate endpoint

--- src/types/oauth.types.ts
+++ src/types/oauth.types.ts
@@ -4,5 +4,30 @@ export type OAuthToken = {
   id_token: string;
   scope: string;
   expires_in: number;
-  token_type: "Bearer";
+  token_type: string;
+};
+
+export type OAuthUserInfo = {
+  sub: string;
+  name: string;
+  given_name: string;
+  family_name: string;
+  middle_name: string;
+  nickname: string;
+  preferred_username: string;
+  profile: string;
+  picture: string;
+  website: string;
+  email: string;
+  email_verified: boolean;
+  gender: string;
+  birthdate: string;
+  zoneinfo: string;
+  locale: string;
+  phone_number: string;
+  phone_number_verified: boolean;
+  address: {
+    country: string;
+  };
+  updated_at: string;
 };
