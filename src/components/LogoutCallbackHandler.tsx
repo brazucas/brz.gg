ca10bf804aba7dd39afe0765d699ef9ceb858b70
pedropapa <pedro.papadopolis@gmail.com>
2023-10-07 13:01:06 +1100

    refactor: changing how tokens are stored

--- src/components/LogoutCallbackHandler.tsx
+++ src/components/LogoutCallbackHandler.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useAuthToken } from "../hooks/useAuthToken";
+import { useUser } from "../hooks/useUser";
 
 export const LogoutCallbackHandler = () => {
     const { removeAuthTokens } = useAuthToken();
+    const { removeUserInfo } = useUser();
 
     useEffect(() => {
         removeAuthTokens();
+        removeUserInfo();
 
         window.location.href = '/';
     }, [])
