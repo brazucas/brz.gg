fb147881af8a2a178e36a339c6c222e292bec8d9
pedropapa <pedro.papadopolis@gmail.com>
2023-10-08 16:26:52 +1100

    feat: auth0client class

---
+++ src/common/oauth/providers/auth0-client.ts
@@ -0,0 +1,12 @@
+import { OAuthClient } from "../../oauth-client";
+
+export class Auth0Client extends OAuthClient {
+  constructor() {
+    super(
+      import.meta.env.PUBLIC_AUTH0_DOMAIN,
+      import.meta.env.PUBLIC_AUTH0_CLIENT_ID,
+      import.meta.env.AUTH0_SECRET,
+      import.meta.env.PUBLIC_AUTH0_LOGOUT_URI
+    );
+  }
+}
